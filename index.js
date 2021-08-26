32fdd89289402d40e9cce9a3be18d50972ddda7b
Sarah Holland <sholland236@gmail.com>
2021-08-26 16:11:19 +0100

    clear cells works for one cell

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // need to put all loose info in classes/objects
-const cells = document.querySelectorAll(".grid__cell");
+// first cell only 'cells'
+const cells = document.querySelector(".cell");
 const restart = document.querySelector(".button__restart");
 
 // Player objects - need to add pieces      !!!
@@ -14,7 +15,7 @@ const playerOne = new Player("Player One", "X");
 const playerTwo = new Player("Player Two", "O");
 
 // create currentPlayer for what piece to add to cell
-const currentPlayer = playerOne;
+let currentPlayer = playerOne;
 
 // create function to add piece to cell - how to select the cell - one for each or queryall??                      use query all - one function to add icon,switch to next player and check for win
 const cellSelected = () => {
@@ -28,9 +29,12 @@ const cellSelected = () => {
 
 // create function for button to clear cells + set player to player 1
 const clearCells = () => {
+    // what about fas class?
     if (cells.classList.contains("fa-cat")) {
+        cells.classList.remove("fas");
         cells.classList.remove("fa-cat");
     } else if (cells.classList.contains("fa-dog")) {
+        cells.classList.remove("fas");
         cells.classList.remove("fa-dog");
     }
     currentPlayer = playerOne;
