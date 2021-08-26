669794c1e9ab28ddc8907d4e985b6bf96b544d3c
Sarah Holland <sholland236@gmail.com>
2021-08-26 16:38:49 +0100

    clearCells working correctly

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // need to put all loose info in classes/objects
 // first cell only 'cells'
-const cells = document.querySelector(".cell");
+const cells = document.querySelectorAll(".cell");
 const restart = document.querySelector(".button__restart");
+
+// how to stop player taken cell already selected??
 
 // Player objects - need to add pieces      !!!
 // not looking at innerHTML, looking at class list
+// can i add "fas fa-cat" for the piece, then access later: if currentPlayer.name === "Player One" cell.classList.addClass(currentPlayer.piece)     ???
 class Player {
     constructor(name, piece) {
         this.name = name;
         this.piece = piece;
     }
 }
-const playerOne = new Player("Player One", "X");
-const playerTwo = new Player("Player Two", "O");
+const playerOne = new Player("Player One", "fas fa-cat");
+const playerTwo = new Player("Player Two", "fas fa-dog");
 
 // create currentPlayer for what piece to add to cell
 let currentPlayer = playerOne;
 
 // create function to add piece to cell - how to select the cell - one for each or queryall??                      use query all - one function to add icon,switch to next player and check for win
 const cellSelected = () => {
-    // 
+    // add piece to sell
+    // check if won
+    // change to other player
 }
 // create function to switch player - is it a function or just set to other player at end of above function?
 
@@ -27,16 +32,17 @@ const cellSelected = () => {
 // create functionality for when player wins
 // create functionality for when no player wins
 
-// create function for button to clear cells + set player to player 1
+// create function - works for only one cell - how to work for others
 const clearCells = () => {
-    // what about fas class?
-    if (cells.classList.contains("fa-cat")) {
-        cells.classList.remove("fas");
-        cells.classList.remove("fa-cat");
-    } else if (cells.classList.contains("fa-dog")) {
-        cells.classList.remove("fas");
-        cells.classList.remove("fa-dog");
-    }
+    cells.forEach(cell => {
+        if (cell.classList.contains("fa-cat")) {
+            cell.classList.remove("fas");
+            cell.classList.remove("fa-cat");
+        } else if (cell.classList.contains("fa-dog")) {
+            cell.classList.remove("fas");
+            cell.classList.remove("fa-dog");
+        }
+    })
     currentPlayer = playerOne;
 };
 restart.addEventListener("click", clearCells);
