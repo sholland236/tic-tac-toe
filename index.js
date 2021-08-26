ec9a8b4871b65f86d7f56c15b5021edf286e629a
Sarah Holland <sholland236@gmail.com>
2021-08-26 15:44:05 +0100

    create player class update clearCells function notes update

--- index.js
+++ index.js
@@ -2,30 +2,37 @@
 const cells = document.querySelectorAll(".grid__cell");
 const restart = document.querySelector(".button__restart");
 
-// create function for button to clear cells + set player to player 1
-const clearCells = () => {
-    cells.innerHTML = "";
-    // ^^incorrect
-    // need to set player to player one
-};
-restart.addEventListener("click", clearCells);
-
-// Player objects - should they  be classes?
-const playerOne = {
-    name: "Player One",
-    // how to add icon
+// Player objects - need to add pieces      !!!
+// not looking at innerHTML, looking at class list
+class Player {
+    constructor(name, piece) {
+        this.name = name;
+        this.piece = piece;
+    }
 }
-const playerTwo = {
-    name: "Player Two",
-    // add icon
-}
+const playerOne = new Player("Player One", "X");
+const playerTwo = new Player("Player Two", "O");
 
 // create currentPlayer for what piece to add to cell
 const currentPlayer = playerOne;
 
-// create function to add piece to cell
-// create function to switch player
+// create function to add piece to cell - how to select the cell - one for each or queryall??                      use query all - one function to add icon,switch to next player and check for win
+const cellSelected = () => {
+    // 
+}
+// create function to switch player - is it a function or just set to other player at end of above function?
 
 // create ways to win
 // create functionality for when player wins
 // create functionality for when no player wins
+
+// create function for button to clear cells + set player to player 1
+const clearCells = () => {
+    if (cells.classList.contains("fa-cat")) {
+        cells.classList.remove("fa-cat");
+    } else if (cells.classList.contains("fa-dog")) {
+        cells.classList.remove("fa-dog");
+    }
+    currentPlayer = playerOne;
+};
+restart.addEventListener("click", clearCells);
