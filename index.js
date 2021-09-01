563e33e39a2daf490c6414e4f9b956dede2985cf
Sarah Holland <sholland236@gmail.com>
2021-09-01 12:30:02 +0100

    tidying

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 // still need to work on colour, + for pieces, _variables file
 
 // need to put all loose info in classes/objects
-const cells = document.querySelectorAll(".cell");
 const cellContainers = document.querySelectorAll(".grid__cell");
 const restart = document.querySelector(".button__restart");
-const cellOne = document.querySelector("#cell-one");
-const cellTwo = document.querySelector("#cell-two");
-const cellThree = document.querySelector("#cell-three");
-const cellFour = document.querySelector("#cell-four");
-const cellFive = document.querySelector("#cell-five");
-const cellSix = document.querySelector("#cell-six");
-const cellSeven = document.querySelector("#cell-seven");
-const cellEight = document.querySelector("#cell-eight");
-const cellNine = document.querySelector("#cell-nine");
 
 // how to stop player taking cell already selected??
 
@@ -58,8 +48,16 @@ cellContainers.forEach((cell) => {
 })
 
 // create ways to win
-// works for cats only at the moment
 const hasWon = () => {
+    const cellOne = document.querySelector("#cell-one");
+    const cellTwo = document.querySelector("#cell-two");
+    const cellThree = document.querySelector("#cell-three");
+    const cellFour = document.querySelector("#cell-four");
+    const cellFive = document.querySelector("#cell-five");
+    const cellSix = document.querySelector("#cell-six");
+    const cellSeven = document.querySelector("#cell-seven");
+    const cellEight = document.querySelector("#cell-eight");
+    const cellNine = document.querySelector("#cell-nine");
     let cellsArray = [[cellOne, cellTwo, cellThree], [cellFour, cellFive, cellSix], [cellSeven, cellEight, cellNine]];
     if (cellsArray[0][0].classList.contains("fa-cat") && cellsArray[0][1].classList.contains("fa-cat") && cellsArray[0][2].classList.contains("fa-cat")) {
         win = true;
@@ -94,17 +92,6 @@ const hasWon = () => {
     } else if (cellsArray[0][2].classList.contains("fa-dog") && cellsArray[1][1].classList.contains("fa-dog") && cellsArray[2][0].classList.contains("fa-dog")) {
         win = true;
     };
-    
-    // winning combinations
-    // [0][0] === [0][1] === [0][2]
-    // [1][0] === [1][1] === [1][2]
-    // [2][0] === [2][1] === [2][2]
-    // [0][0] === [1][0] === [2][0]
-    // [0][1] === [1][1] === [2][1]
-    // [0][2] === [1][2] === [2][2]
-    // [0][0] === [1][1] === [2][2]
-    // [0][2] === [1][1] === [0][2]
-    // loop?
 }
 
 
@@ -113,6 +100,7 @@ const hasWon = () => {
 
 // clear cells function
 const clearCells = () => {
+    const cells = document.querySelectorAll(".cell");
     cells.forEach(cell => {
         if (cell.classList.contains("fa-cat")) {
             cell.classList.remove("fas");
